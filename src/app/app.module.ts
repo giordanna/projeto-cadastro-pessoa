f78106a6dcdbeda1b5d2a640dbfac7505dea4489
Giordanna De Gregoriis <gior.grs@gmail.com>
2018-11-09 16:11:16 -0300

    feat: Validação de formulário implementada
    Agora para registrar é necessário que os dados sejam inseridos da maneira incorreta.
    
    Falta: alertar a outra tela que o usuário foi inserido, implementar filtro.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,7 +29,8 @@ import { ApiService } from './api.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
--- src/app/criar-pessoa/criar-pessoa.component.ts
+++ src/app/criar-pessoa/criar-pessoa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService} from '../api.service';
-import {Router} from '@angular/router';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
+import {FormGroup, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pessoa',
@@ -8,22 +9,31 @@ import {Router} from '@angular/router';
   styleUrls: ['./criar-pessoa.component.css']
 })
 export class CriarPessoaComponent implements OnInit {
-  novaPessoa: {} = {
-    nome: 'Fulano de Tal',
-    idade: '29',
-    email: 'fulano@exemplo.com',
-    login: 'fulano.29',
-    senha: '123qwe',
-    status: false
-  };
+  /* consultado: https://scotch.io/tutorials/angular-2-form-validation */
+  form: FormGroup;
 
-  constructor(public api: ApiService, private router: Router) { }
+  constructor(public api: ApiService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      idade: ['', [Validators.required, Validators.min(18), Validators.max(100)]],
+      email: ['', [Validators.required, Validators.email]],
+      /* consultado: https://stackoverflow.com/questions/1221985/how-to-validate-a-user-name-with-regex */
+      login: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[a-zA-Z0-9]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$')]
+      ],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      status: ['']
+    });
   }
 
+  get f() { return this.form.controls; }
+
   onAdicionarPessoa() {
-    this.api.createPessoa(this.novaPessoa).subscribe((result) => {
+    this.api.createPessoa(this.form.value).subscribe((result) => {
       this.router.navigate(['/lista']);
     }, (err) => {
       console.log(err);
